2efd3d37fd9ff6f5f6054e94c08e09c09059de40
HubertBer <hubert.bernacki@student.uj.edu.pl>
2025-03-29 19:33:16 +0100

    simple bullets + interpolation fix

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,13 @@ Vector2 GetRandomVector2(){
     return Vector2Normalize({(float)GetRandomValue(-500, 500), (float)GetRandomValue(-500, 500)});
 }
 
+struct bullet{
+    Vector2 prev_pos;
+    Vector2 pos;
+    Vector2 velocity;
+    constexpr static float max_speed = 800.f;
+};
+
 struct enemy{
     Vector2 prev_pos;
     Vector2 pos;
@@ -24,6 +31,7 @@ struct game_data{
     Vector2 prev_player_pos = {100.0f, 100.0f};
     Vector2 player_velocity = {0, 0};
     std::vector<enemy> enemies {};
+    std::vector<bullet> bullets {};
 
     const float player_speed = 500.0f;
 };
@@ -34,14 +42,19 @@ void physics_tick(game_data& game){
     game.player_pos += game.player_velocity * physics_dt;
 
     // Enemies
-    
-    
     for(enemy& enem : game.enemies){
         enem.prev_pos = enem.pos;
-        if(GetRandomValue(0, 99) == 0){
-            enem.velocity = GetRandomVector2() * enemy::max_speed;
-        }
+        enem.velocity = Vector2Normalize(game.player_pos - enem.pos) * enemy::max_speed;
+        // if(GetRandomValue(0, 99) == 0){
+        //     enem.velocity = GetRandomVector2() * enemy::max_speed;
+        // }
         enem.pos += enem.velocity * physics_dt;
+    }
+
+    // Bullets
+    for(bullet& b : game.bullets){
+        b.prev_pos = b.pos;
+        b.pos += b.velocity * physics_dt;
     }
 }
 
@@ -76,6 +89,20 @@ int main() {
             game.player_velocity *= game.player_speed;
         }
 
+        if(IsKeyPressed(KEY_SPACE)){
+            Vector2 bullet_vel = (game.player_pos - game.prev_player_pos); //Later probably just based on rotation
+            if(Vector2LengthSqr(bullet_vel) < EPSILON){
+                bullet_vel = {1.0, 0.0f}; 
+            }
+            bullet_vel = Vector2Normalize(bullet_vel) * bullet::max_speed;
+
+            game.bullets.push_back({
+                game.player_pos,
+                game.player_pos,
+                bullet_vel,
+            });
+        }
+
         // PHYSICS SIMULATION - CONSTANT FRAME TIME SIMULATION
         float dt = GetFrameTime();
         game_time += dt;
@@ -92,12 +119,17 @@ int main() {
         ClearBackground(BLUE);
         
         // Rember that positions are in the physics simulation which is slightly ahead, we need to interpolate between current position and the previous position 
-        Vector2 player_pos_now = Vector2Lerp(game.prev_player_pos, game.player_pos, (physics_time - game_time)/physics_dt);
+        Vector2 player_pos_now = Vector2Lerp(game.prev_player_pos, game.player_pos, 1 - (physics_time - game_time)/physics_dt);
         DrawCircle(static_cast<int>(player_pos_now.x), static_cast<int>(player_pos_now.y), 30, RED);
 
         for(enemy enem : game.enemies){
-            Vector2 enem_pos_now = Vector2Lerp(enem.prev_pos, enem.pos, (physics_time - game_time)/physics_dt);        
+            Vector2 enem_pos_now = Vector2Lerp(enem.prev_pos, enem.pos, 1 - (physics_time - game_time)/physics_dt);        
             DrawRectangle(static_cast<int>(enem_pos_now.x), static_cast<int>(enem_pos_now.y), 20, 40, GREEN);
+        }
+        
+        for(bullet b : game.bullets){
+            Vector2 bullet_pos_now = Vector2Lerp(b.prev_pos, b.pos, 1 - (physics_time - game_time)/physics_dt);        
+            DrawRectangle(static_cast<int>(bullet_pos_now.x), static_cast<int>(bullet_pos_now.y), 5, 20, YELLOW);
         }
 
         EndDrawing();
