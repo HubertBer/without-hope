c66cc65ae43d05245660390bdd20686aeb32f409
Maciej Brzozowski <maciek.brzozowski149@gmail.com>
2025-05-30 16:09:59 +0200

    collision circleoutline

--- src/collider/CollisionSystem.cpp
+++ src/collider/CollisionSystem.cpp
@@ -6,6 +6,23 @@
 #include <list>
 
 namespace{
+    bool CheckCollisionCircleOutlineCircle(Vector2 center1, float radius1,float radius_unimportant, Vector2 center2, float radius2)
+    {
+        bool collision = false;
+
+        float dx = center2.x - center1.x;      // X distance between centers
+        float dy = center2.y - center1.y;      // Y distance between centers
+
+        float distanceSquared = dx*dx + dy*dy; // Distance between centers squared
+        float radiusSum = radius1 + radius2;
+
+        collision = (distanceSquared <= (radiusSum*radiusSum))&& (distanceSquared >= radius_unimportant*radius_unimportant);
+
+        return collision;
+    }
+}
+
+namespace{
     bool AreColliding(Collider col1, Collider col2){
         if(col1.type > col2.type){
             std::swap(col1, col2);
